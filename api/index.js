d8a355de9263dd7f7eab98f25617f0ce6de57d00
sara2829 <2021bec102@gmail.com>
2023-10-24 00:40:04 +0530

    connecting  upto mongo db

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app=express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+})
 
 app.listen(3000,(req,res) =>{
     console.log('Server is Listening on port 3000')
 })
-app.get()
+
+
+
 
