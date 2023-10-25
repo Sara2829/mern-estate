34abf594d2b598bcb9a5f5b48a8acb5b2a1a5b12
sara2829 <2021bec102@gmail.com>
2023-10-25 17:57:35 +0530

    cretae a signup api

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 const app=express();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
@@ -13,7 +15,9 @@ app.listen(3000,(req,res) =>{
     console.log('Server is Listening on port 3000')
 })
 
-app.use("/api/user/", userRouter)
+app.use("/api/user/", userRouter);
+app.use("/api/auth/", authRouter);
+
 
 
 
